fb01e3975ca24b930a831f0c3f92cf0de08fdfda
iambillmccann <bill.mccann@gmail.com>
2020-03-18 18:53:39 -0400

    Applying update in order to revert

--- projecteuler/problems/LargestPalindromeProduct.py
+++ projecteuler/problems/LargestPalindromeProduct.py
@@ -20,6 +20,10 @@ class LargestPalindromeProduct(IEulerSolution):
         for number1 in range (100, 1000):
             for number2 in range (100, 1000):
                 product = number1 * number2
+#                product_as_string = str(product)
+#                if product_as_string[0] == product_as_string[~0]:
+#                    if (product_as_string == product_as_string[:: ~0]) & (product > result):
+#                        result = product
                 if (MathLibrary.reverse_digits(product) == product) & (product > result):
                     result = product
 
